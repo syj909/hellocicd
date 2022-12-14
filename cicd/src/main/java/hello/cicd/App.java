08fbf5226296d32e4e8a5da4f0e911145c3448a1
syj909 <syj909@github.com>
2022-12-14 18:02:39 +0900

    첫번째 커밋

---
+++ cicd/src/main/java/hello/cicd/App.java
@@ -0,0 +1,14 @@
+package hello.cicd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello CI/CD!" );
+    }
+}
